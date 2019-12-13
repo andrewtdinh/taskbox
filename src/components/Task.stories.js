3664fc635c7ae331f649ef7f8122dc340742e838
Andrew Dinh <adinh@sightmachine.com>
2019-12-13 14:57:48 -0800

    Update Task stories and snapshots

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -1,27 +1,28 @@
 import React from 'react';
+import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
 import Task from './Task';
 
-const task = {
+export const task = {
   id: '1',
   title: 'Test Task',
   state: 'TASK_INBOX',
   updatedAt: new Date(2018, 0, 1, 9, 0),
 };
 
-const actions = {
+export const actions = {
   onPinTask: action('onPinTask'),
   onArchiveTask: action('onArchiveTask'),
 };
 
-// storiesOf('Task', module)
-//   .add('default', () => <Task task={task} {...actions} />)
-//   .add('pinned', () => <Task task={{ ...task, state: 'TASK_PINNED' }} {...actions} />)
-//   .add('archived', () => <Task task={{ ...task, state: 'TASK_ARCHIVED' }} {...actions} />);
+storiesOf('Task', module)
+  .add('default', () => <Task task={task} {...actions} />)
+  .add('pinned', () => <Task task={{ ...task, state: 'TASK_PINNED' }} {...actions} />)
+  .add('archived', () => <Task task={{ ...task, state: 'TASK_ARCHIVED' }} {...actions} />);
 
-export default { title: 'Task'};
+// export default { title: 'Task'};
 
-export const withInboxState = () => <Task task={task} {...actions} />
-export const withPinnedState = () => <Task task={{ ...task, state: 'TASK_PINNED' }} {...actions} />
-export const withArchivedState = () => <Task task={{ ...task, state: 'TASK_ARCHIVED' }} {...actions} />
+// export const withInboxState = () => <Task task={task} {...actions} />
+// export const withPinnedState = () => <Task task={{ ...task, state: 'TASK_PINNED' }} {...actions} />
+// export const withArchivedState = () => <Task task={{ ...task, state: 'TASK_ARCHIVED' }} {...actions} />
