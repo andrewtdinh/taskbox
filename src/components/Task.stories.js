fb2550e06cd06fc721c60028ae9a84cfe57d744e
Andrew Dinh <adinh@sightmachine.com>
2019-12-13 11:04:24 -0800

    Use different way to define stories

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -4,19 +4,25 @@ import { action } from '@storybook/addon-actions';
 
 import Task from './Task';
 
-export const task = {
+const task = {
   id: '1',
   title: 'Test Task',
   state: 'TASK_INBOX',
   updatedAt: new Date(2018, 0, 1, 9, 0),
 };
 
-export const actions = {
+const actions = {
   onPinTask: action('onPinTask'),
   onArchiveTask: action('onArchiveTask'),
 };
 
-storiesOf('Task', module)
-  .add('default', () => <Task task={task} {...actions} />)
-  .add('pinned', () => <Task task={{ ...task, state: 'TASK_PINNED' }} {...actions} />)
-  .add('archived', () => <Task task={{ ...task, state: 'TASK_ARCHIVED' }} {...actions} />);
+// storiesOf('Task', module)
+//   .add('default', () => <Task task={task} {...actions} />)
+//   .add('pinned', () => <Task task={{ ...task, state: 'TASK_PINNED' }} {...actions} />)
+//   .add('archived', () => <Task task={{ ...task, state: 'TASK_ARCHIVED' }} {...actions} />);
+
+export default { title: 'Task'};
+
+export const withInboxState = () => <Task task={task} {...actions} />
+export const withPinnedState = () => <Task task={{ ...task, state: 'TASK_PINNED' }} {...actions} />
+export const withArchivedState = () => <Task task={{ ...task, state: 'TASK_ARCHIVED' }} {...actions} />
