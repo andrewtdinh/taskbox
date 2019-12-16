08c7685b4a96e185698f7e1774d9089cf80e4550
Andrew Dinh <adinh@sightmachine.com>
2019-12-16 11:32:32 -0800

    Connect real store to PureInboxScreen

--- src/components/InboxScreen.stories.js
+++ src/components/InboxScreen.stories.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
 import { Provider } from 'react-redux';
 
 import { PureInboxScreen } from './InboxScreen';
-import { defaultTasks } from './TaskList.stories';
 
-// A super-simple mock of a redux store
-const store = {
-  getState: () => {
-    return {
-      tasks: defaultTasks,
-    };
-  },
-  subscribe: () => 0,
-  dispatch: action('dispatch'),
-};
+import store from '../lib/redux';
 
 storiesOf('InboxScreen', module)
   .addDecorator(story => <Provider store={store}>{story()}</Provider>)
